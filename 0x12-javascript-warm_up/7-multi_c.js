552da07eef90a86f037c41eb59733cbabcda66a2
oshybobo <oshinnaiye@gmail.com>
2024-04-26 08:30:20 -0700

    java

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@ const args = process.argv.slice(2);
 const firstArg = args[0];
 const number = parseInt(firstArg, 10);
 if (isNaN(number) || number < 0) {
-    console.log('Missing number of occurences');
+  console.log('Missing number of occurences');
 } else {
-    for (let i = 0; i < number; i++) {
-        console.log('C is fun');
-    }
+  for (let i = 0; i < number; i++) {
+    console.log('C is fun');
+  }
 }
